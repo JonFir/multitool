4badd6bfbf97f777f2b6e8182d9a6b63ee4b66d8
JonFir <yolchev@gmail.com>
2026-02-11 10:57:37 +0700

    add gui

---
+++ gui/multitool/multitoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  multitoolApp.swift
+//  multitool
+//
+//  Created by Евгений Елчев on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct multitoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
